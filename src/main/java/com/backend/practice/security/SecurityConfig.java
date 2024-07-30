5f060cc4e5fb01d0826b1623935eb6958878daed
cgb12121 <quanbaoyb@gmail.com>
2024-07-30 15:22:22 +0700

    ...

--- src/main/java/com/backend/practice/security/SecurityConfig.java
+++ src/main/java/com/backend/practice/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import com.backend.practice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
